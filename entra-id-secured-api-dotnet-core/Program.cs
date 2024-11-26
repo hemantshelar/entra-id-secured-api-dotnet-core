6a3bdfea79cfa39061a3955dd4422bd6f90acafa
Hemant <hemant.shelar@gmail.com>
2024-11-26 21:48:09 +1100

    Working graph client.

--- entra-id-secured-api-dotnet-core/Program.cs
+++ entra-id-secured-api-dotnet-core/Program.cs
@@ -1,6 +1,9 @@
+using Azure.Identity;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Graph;
 using Microsoft.Identity.Abstractions;
+using Microsoft.Identity.Client;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.Resource;
 
@@ -10,7 +13,7 @@ namespace entra_id_secured_api_dotnet_core
 	{
 		public static void Main(string[] args)
 		{
-			var builder = WebApplication.CreateBuilder(args);
+			var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
 
 			// Add services to the container.
 			builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -20,6 +23,16 @@ namespace entra_id_secured_api_dotnet_core
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
+			var tenantId = builder.Configuration["AzureAd:TenantId"];
+			var clientId = builder.Configuration["AzureAd:ClientId"];
+			var clientSecret = builder.Configuration["AzureAd:ClientSecret"];
+			#region Inject Microsoft Graph
+			var clientSecretCredential = new ClientSecretCredential(
+tenantId, clientId, clientSecret);
+			var scopes = new[] { "https://graph.microsoft.com/.default" };
+			var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
+			builder.Services.AddSingleton<GraphServiceClient>(graphClient);
+			#endregion
 
 			var app = builder.Build();
 
@@ -32,7 +45,7 @@ namespace entra_id_secured_api_dotnet_core
 
 			app.UseHttpsRedirection();
 
-   app.UseAuthentication();
+			app.UseAuthentication();
 
 			app.UseAuthorization();
 
